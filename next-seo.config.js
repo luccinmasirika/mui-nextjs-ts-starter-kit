247b0abfbcc4d2fedde51e2b5f073729149ebcb8
Luccin Masirika <luccinmasirika@gmail.com>
2022-03-12 22:11:27 +0100

    husky

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  openGraph: {
+    type: 'website',
+    locale: 'fr',
+    url: 'https://ingeniouscity.com',
+    site_name: 'Ingenious City',
+  },
+  twitter: {
+    handle: 'Luccin Masirika',
+    site: 'Ingenious City',
+    cardType: 'summary_large_image',
+  },
+};
--- src/components/starter/Starter.tsx
+++ src/components/starter/Starter.tsx
@@ -6,17 +6,17 @@
  * @desc [Home Page]
  */
 
-import { useI18n } from '@/hooks/useI18n';
-import { Button, Container, Stack, Typography } from '@mui/material';
-import dayjs from 'dayjs';
-import 'dayjs/locale/fr';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import type { NextPage } from 'next';
-import { useDarkMode } from 'next-dark-mode';
-import { useRouter } from 'next/router';
-import * as React from 'react';
+import { useI18n } from '@/hooks/useI18n'
+import { Button, Container, Stack, Typography } from '@mui/material'
+import dayjs from 'dayjs'
+import 'dayjs/locale/fr'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import type { NextPage } from 'next'
+import { useDarkMode } from 'next-dark-mode'
+import { useRouter } from 'next/router'
+import * as React from 'react'
 
-dayjs.extend(relativeTime);
+dayjs.extend(relativeTime)
 
 const Starter: NextPage = () => {
   const {
@@ -26,24 +26,24 @@ const Starter: NextPage = () => {
     switchToAutoMode,
     switchToDarkMode,
     switchToLightMode,
-  } = useDarkMode();
+  } = useDarkMode()
 
-  const { locale } = useRouter();
-  const switchLanguage = useI18n<'fr' | 'en'>();
+  const { locale } = useRouter()
+  const switchLanguage = useI18n<'fr' | 'en'>()
 
   const toggleMode = () => {
-    darkModeActive ? switchToLightMode() : switchToDarkMode();
-  };
+    darkModeActive ? switchToLightMode() : switchToDarkMode()
+  }
 
   const toggleLanguage = () => {
-    locale === 'en' ? switchLanguage('fr') : switchLanguage('en');
-  };
+    locale === 'en' ? switchLanguage('fr') : switchLanguage('en')
+  }
 
   return (
     <Container>
       <Stack
-        justifyContent='center'
-        alignItems='center'
+        justifyContent="center"
+        alignItems="center"
         sx={{
           bgcolor: 'secondary.main',
           my: 8,
@@ -53,54 +53,54 @@ const Starter: NextPage = () => {
         }}
       >
         <Typography
-          variant='h3'
-          component='h1'
-          color='primary'
+          variant="h3"
+          component="h1"
+          color="primary"
           gutterBottom
           fontWeight={700}
-          textAlign='center'
+          textAlign="center"
         >
           MUI NextJs with TypeScript Starter Kit
         </Typography>
-        <Typography color='text.secondary' gutterBottom textAlign='center'>
+        <Typography color="text.secondary" gutterBottom textAlign="center">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum qui et
           quidem quos dolore illo culpa quasi expedita consequuntur corrupti
           minima, nulla quaerat magnam tempore a alias aliquam repudiandae
           saepe!
         </Typography>
 
-        <Typography gutterBottom textAlign='center' color='text.secondary'>
+        <Typography gutterBottom textAlign="center" color="text.secondary">
           Date 1 :{' '}
           {dayjs()
             .locale(locale as string)
             .to(new Date())}
         </Typography>
 
-        <Typography gutterBottom textAlign='center' color='text.secondary'>
+        <Typography gutterBottom textAlign="center" color="text.secondary">
           Date 2 :{' '}
           {dayjs(new Date())
             .locale(locale as string)
             .format('DD MMMM YYYY')}
         </Typography>
 
-        <Typography gutterBottom textAlign='center' color='text.secondary'>
+        <Typography gutterBottom textAlign="center" color="text.secondary">
           Date 3 :{' '}
           {dayjs(new Date())
             .locale(locale as string)
             .format('dddd - DD MMMM YYYY / HH:MM')}
         </Typography>
 
-        <Stack direction='row' spacing={2} sx={{ my: 4 }}>
-          <Button onClick={toggleMode} variant='contained' color='accent'>
+        <Stack direction="row" spacing={2} sx={{ my: 4 }}>
+          <Button onClick={toggleMode} variant="contained" color="accent">
             Switch to {darkModeActive ? 'Light' : 'Dark'} theme
           </Button>
-          <Button onClick={toggleLanguage} variant='contained' color='primary'>
+          <Button onClick={toggleLanguage} variant="contained" color="primary">
             Switch to {locale === 'en' ? 'French' : 'English'}
           </Button>
         </Stack>
       </Stack>
     </Container>
-  );
-};
+  )
+}
 
-export default Starter;
+export default Starter
