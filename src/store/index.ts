ede3d2c3ceb360b33297c4d3ee1f60a753d0e491
Luccin Masirika <luccinmasirika@gmail.com>
2022-03-13 03:57:07 +0100

    fix bug typo

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,9 @@ declare global {
   }
 }
 
-const composeEnhancers =window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose
 
 export default createStore(reducers, composeEnhancers())
